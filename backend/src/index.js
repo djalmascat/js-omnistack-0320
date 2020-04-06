062974156a48e31efeb188c3762866ec3c7060ba
Allan Lessa <djalmascat@gmail.com>
2020-04-06 19:02:18 -0300

    Frontend finished 3-1:14

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,9 +4,6 @@ const routes = require('./routes');
 
 const app = express();
 
-//app.use(cors({
-    //origin: 'http://www.meuapp.com'
-//}));
 app.use(cors);
 app.use(express.json());
 app.use(routes);
