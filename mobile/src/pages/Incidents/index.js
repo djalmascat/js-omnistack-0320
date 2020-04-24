568371ccd5b11e9dddcd4611940d92f516e82773
Allan Lessa <djalmascat@gmail.com>
2020-04-24 19:48:06 -0300

    Bugfix: Incidents page

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -37,7 +37,7 @@ export default function Incidents(){
             param: { page }
         });
 
-        setIncidents([...incidents. ...response.data]);
+        setIncidents([...incidents, ...response.data]);
         setTotal(response.headers['x-total-count']);
         setPage(page + 1);
         setLoading(false);
